2db52440b6aa8539c04d471eea52c85b192da7b8
Digital768 <121376732+Digital768@users.noreply.github.com>
2024-07-16 15:28:41 +0300

    spaceships with wrap animation

--- frontend/src/components/space/Spaceship.jsx
+++ frontend/src/components/space/Spaceship.jsx
@@ -1,19 +1,16 @@
 import React, { useState, useEffect } from "react";
 import HumanSpaceship from "/spaceship_4.png";
-
+import styles from "./spaceship.module.css";
 function Spaceship({ Radius, Percentage, centerX, centerY }) {
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     const minRadius = Radius + 50;
-    // Calculate the radius of the smaller circle
-    const smallerRadius = (((100 - Percentage) / 100) * Radius)+ 100;
-    console.log(smallerRadius)
+    const smallerRadius = ((100 - Percentage) / 100) * Radius + 100;
+    console.log(smallerRadius);
 
-    // Generate a random angle
     const randomAngle = Math.random() * 2 * Math.PI;
 
-    // Calculate the position on the smaller circle
     const x = centerX + smallerRadius * Math.cos(randomAngle);
     const y = centerY + smallerRadius * Math.sin(randomAngle);
 
@@ -22,23 +19,24 @@ function Spaceship({ Radius, Percentage, centerX, centerY }) {
 
   return (
     <div
-      className="spaceship-container"
+      className={styles.spaceshipContainer}
       style={{
-        transform: "translate(-50%,-50%)",
-        position: "absolute",
-        left: `${position.x}px`,
-        top: `${position.y}px`,
-        zIndex: 1,
+        "--postionX": position.x + "px",
+        "--postionY": position.y + "px",
       }}
     >
-      <img
-        draggable={false}
-        src={HumanSpaceship}
-        alt="Spaceship"
-        style={{
-          width: "100px",
-        }}
-      />
+      <div className={styles.container}>
+        <img
+          className={styles.spaceship}
+          draggable={false}
+          src={HumanSpaceship}
+          alt="Spaceship"
+          style={{
+            width: "100px",
+          }}
+        />
+        <div className={styles.portal} />
+      </div>
     </div>
   );
 }
